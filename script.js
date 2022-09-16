aaf197e555691ababb895681ddd07afc32da42da
madspecco <bogdan.bolos30@gmail.com>
2022-09-16 11:22:11 +0300

    Created operate Function

--- script.js
+++ script.js
@@ -21,3 +21,9 @@ const divide = function (operand1, operand2) {
 }
 
 console.log(divide(9,4));
+
+function operate(operator, op1, op2) {
+    return operator(op1, op2);
+}
+
+console.log(operate(subtract, 15, 3));
