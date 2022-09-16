bdd19d7624993202dd82367af7a1a2fa164e3066
madspecco <bogdan.bolos30@gmail.com>
2022-09-16 11:18:57 +0300

    Created Basic Math Functions

---
+++ script.js
@@ -0,0 +1,23 @@
+const add = function(operand1, operand2) {
+    return operand1 + operand2;
+}
+
+console.log(add(2,3));
+
+const subtract = function (operand1, operand2) {
+    return operand1 - operand2;
+}
+
+console.log(subtract(100, 50));
+
+const multiply = function (operand1, operand2) {
+    return operand1 * operand2;
+}
+
+console.log(multiply(10, 10));
+
+const divide = function (operand1, operand2) {
+    return operand1 / operand2;
+}
+
+console.log(divide(9,4));
